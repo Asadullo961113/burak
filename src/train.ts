ee51bba9ade3c337abf5069ebb748b6bfca73fc9
Asadullo961113 <asadillok9@gmail.com>
2024-09-05 09:00:35 +0900

    feat: train area

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,31 @@
-function highestIndex(arr: number[]): number {
+/*
+MIT 18 H-TASK: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+*/ 
+
+function musbatSon(arr:number[]) : string {
+    const a = arr.filter(function (i) {
+        return i > 0;
+    })
+    return a.join('');
+};    
+
+// Misol uchun:
+console.log(musbatSon([-45, -3, -5, 7, -32])); // javob : 7
+
+
+
+
+/*MIT 18 G-TASK: 
+
+Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.*/
+
+
+/*function highestIndex(arr: number[]): number {
     if (arr.length === 0) {
         return -1;
     }
@@ -15,4 +42,4 @@ function highestIndex(arr: number[]): number {
 }
 
 // Misol uchun:
-console.log(highestIndex([45, 3, 5, 7, 32])); // 0
+console.log(highestIndex([45, 3, 5, 7, 32])); // 0 */
